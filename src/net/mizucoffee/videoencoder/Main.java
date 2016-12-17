17cecc13c81f05b23af0aaa7ce901fe636404ed2
MizuCoffee <mizucoffee@MizuCoffees-MacBook-Pro.local>
2016-12-17 15:28:11 +0900

    [fix] path

--- src/net/mizucoffee/videoencoder/Main.java
+++ src/net/mizucoffee/videoencoder/Main.java
@@ -42,7 +42,7 @@ public class Main {
 
                 Runtime runtime = Runtime.getRuntime();
                 try {
-                    Process p = runtime.exec("ffmepg -i " + files[0].getPath() + " " + folder2 + files[0].getName().split(",")[0] + ".mp4");
+                    Process p = runtime.exec("ffmepg -i " + files[0].getPath() + " " + folder2 + "¥" + files[0].getName().split(",")[0] + ".mp4");
                     p.waitFor();
                 } catch (IOException e) {
                     e.printStackTrace();
