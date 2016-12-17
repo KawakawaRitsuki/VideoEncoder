1d2c2afbe34ba1fd1bd06e8db47e5af51f1352c0
MizuCoffee <mizucoffee@MizuCoffees-MacBook-Pro.local>
2016-12-17 17:53:39 +0900

    [fix] command

--- src/net/mizucoffee/videoencoder/Main.java
+++ src/net/mizucoffee/videoencoder/Main.java
@@ -40,14 +40,14 @@ public class Main {
             if(files.length != 0) {
                 files[0].getPath();
 
-                Runtime runtime = Runtime.getRuntime();
                 try {
 
                     System.out.println("エンコード開始");
                     System.out.println("ファイル名:" + files[0].getName());
                     System.out.println("コマンド:" + "cd \"" + folder2.getPath() + "\"&ffmpeg -i \"" + files[0].getPath() + "\" \"" + files[0].getName().split("\\.")[0] + ".mp4\"");
-                    runtime.exec("cd \"" + folder2.getPath() + "\"");
-                    Process p = runtime.exec("ffmpeg -i \"" + files[0].getPath() + "\" \"" + files[0].getName().split("\\.")[0] + ".mp4\"");
+                    ProcessBuilder pb = new ProcessBuilder("ffmpeg -i \"" + files[0].getPath() + "\" \"" + files[0].getName().split("\\.")[0] + ".mp4\"");
+                    pb.directory(new File(folder2.getPath()));
+                    Process p = pb.start();
 
                     p.waitFor();
                     p.destroy();
