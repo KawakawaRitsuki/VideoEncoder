ede64aa56bb916470333dd566ebfd4888261b7a0
MizuCoffee <mizucoffee@MizuCoffees-MacBook-Pro.local>
2016-12-17 20:09:32 +0900

    [fix] command

--- src/net/mizucoffee/videoencoder/Main.java
+++ src/net/mizucoffee/videoencoder/Main.java
@@ -44,8 +44,11 @@ public class Main {
                     System.out.println("エンコード開始");
                     System.out.println("ファイル名:" + files[0].getName());
                     System.out.println("コマンド:" + "move "+files[0].getPath() + " " + files[0].getParent()+ "\\0.flv");
-                    Runtime.getRuntime().exec("cmd.exe /c move "+files[0].getPath() + " " + files[0].getParent()+ "\\0.flv");
-                    ProcessBuilder pb = new ProcessBuilder("ffmpeg" ,"-i", files[0].getParent()+ "\\0.flv" , files[0].getName().split("¥¥.")[0] + ".mp4");
+                    ProcessBuilder mv = new ProcessBuilder("cmd.exe", "/c", "move", files[0].getPath() , files[0].getParent() + "\\0.flv");
+                    mv.directory(new File(folder1.getPath()));
+                    mv.start();
+
+                    ProcessBuilder pb = new ProcessBuilder("ffmpeg" ,"-i", files[0].getParent()+ "\\0.flv" , files[0].getName().split("\\.")[0] + ".mp4");
                     pb.directory(new File(folder2.getPath()));
                     pb.inheritIO();
                     System.out.println("コマンド:" + pb.command());
